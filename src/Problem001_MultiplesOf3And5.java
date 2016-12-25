e4bf400fb7556b400bd7203ab191d7b30b8abda1
Timo Strating <Timostrating@home.nl>
2016-12-25 10:03:52 +0100

    Problem 1 explained

--- src/Problem001_MultiplesOf3And5.java
+++ src/Problem001_MultiplesOf3And5.java
@@ -4,16 +4,13 @@
 public class Problem001_MultiplesOf3And5 {
     public static void main(String args[]) {
 
-        int i = 999;
-        int sum = 0;
+        int max = 1000;                             // How many iterations we will do at max
+        int sum = 0;                                // here we store the total sum
 
-        do {
-            if(i % 3 == 0 || i % 5 == 0) {
-                sum += i;
-            }
-            i--;
-        } while (i > 1);
+        for (int i=1; i < max; i++)                 // loop 1 to max
+            if (i % 3 == 0 || i % 5 == 0)               // if we divide by 3 or 5 and it is a perfect division
+                sum += i;                                   // we add our current index to the total sum
 
-        System.out.println(sum);
+        System.out.println(sum);                    // print the total sum  =  233168
     }
 }
