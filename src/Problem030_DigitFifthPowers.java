8103bbf78d73add795014444ed0d0de6c21d4548
Timo Strating <Timostrating@home.nl>
2017-01-04 23:47:21 +0100

    shorter + explained

--- src/Problem030_DigitFifthPowers.java
+++ src/Problem030_DigitFifthPowers.java
@@ -2,27 +2,16 @@
  * Created by Sneeuwpopsneeuw on 09-Aug-16.
  */
 public class Problem030_DigitFifthPowers {
-
-
     public static void main(String args[]) {
-        final byte FIVE = 5; // five six
         int ans = 0;
 
-        for (int i = 2; i < 999999; i++) {
-
+        for (int i = 2; i < 999999; i++) {  // while true
             int value = 0;
             String numberString = "" + i;
-            for (int j = 0; j < numberString.length(); j++) {
-                value += Math.pow( Character.getNumericValue( numberString.charAt(j) ), FIVE);
-            }
-            if ( value == i ) {
-                ans += value;
-                System.out.println(value);
-            }
-
+            for (int j = 0; j < numberString.length(); j++)   // loop each char in our numberString
+                value += Math.pow( Character.getNumericValue( numberString.charAt(j) ), 5);     // we add this char ^ 5 to the total value
+            if ( value == i )   // if our total value == our current number
+                System.out.println( ans += value );     // bad way to do this but it works
         }
-
-        System.out.println("");
-        System.out.println(ans);
     }
 }
