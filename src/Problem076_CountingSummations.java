b78f346f76bf093aa4de2f42b23cebd174ab022a
Timo Strating <Timostrating@home.nl>
2017-01-05 23:31:44 +0100

    tried Problem 76 recursive

--- src/Problem076_CountingSummations.java
+++ src/Problem076_CountingSummations.java
@@ -1,12 +1,32 @@
 /**
  * Created by Sneeuwpopsneeuw on 26-Aug-16.
  */
-public class Problem076_CountingSummations {  // TODO:   31  -->  76  -->  77  -->  78
+public class Problem076_CountingSummations {  // TODO:   76  -->  77  -->  78
+    public static int[] coinValues = new int[0];
+
     public static void main(String[] args) {
-        int ans = 0;
+        coinValues = new int[100];
+        for (int i=0; i < 100; i++) {
+            coinValues[i] = i;
+        }
+        System.out.println(numWays(100, 100));
+    }
+    static int numWays(int p, int maxCoin){
+        if(p == 1)
+            return 1;
+        else if(p < 0)
+            return 0;
+
+        int ways = 0;
+        for(int i=0; i <= 100  &&  coinValues[i] <= maxCoin; i++){
+            ways += numWays(p - coinValues[i], coinValues[i]);
+        }
+        return ways;
+    }
+}
 
 
-        /*
+/*
 
         for (int a=100; a>0; a-=100) {
             for (int b=a; b>=0; b-=99) {
@@ -22,11 +42,3 @@ public class Problem076_CountingSummations {  // TODO:   31  -->  76  -->  77  -
         This must be made smart   while + array ?
 
         */
-
-
-
-
-
-        System.out.println("ans = " + ans);
-    }
-}
