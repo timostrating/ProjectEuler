27abdbb437a5cb3d9bc96b95ff9ecb1ecfe07e7f
Timo Strating <timostrating@home.nl>
2017-02-05 01:40:05 +0100

    looked at Problem 172

---
+++ src/Problem172_NumbersWithFewRepeatedDigits.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Sneeuwpopsneeuw on 15-Jan-17.
+ */
+public class Problem172_NumbersWithFewRepeatedDigits {
+    public void main(String[] args) {
+        /*
+
+
+
+            9 * 10^17
+
+            111
+            222
+            333
+            444
+            555
+            666
+            777
+            888
+            999
+            000
+
+            maximal combination formula = (N!) / R!(N-R)!
+
+
+         */
+    }
+}
