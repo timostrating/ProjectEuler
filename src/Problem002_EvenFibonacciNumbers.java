5aba62d69d549c7e5c3e369fc5e0dca648565e3c
Timo Strating <timostrating@home.nl>
2017-03-28 16:02:29 +0200

    cleaned up old era

--- src/Problem002_EvenFibonacciNumbers.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Sneeuwpopsneeuw on 23-Feb-16.
- */
-public class Problem002_EvenFibonacciNumbers {
-    public static void main (String args[]) {
-        System.out.println("antwoord = school");
-    }
-}
--- src/Problem013_LargeSum.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Sneeuwpopsneeuw on 02-Apr-16.
- */
-public class Problem013_LargeSum {
-    public static void main(String args[]) {
-        // bruteforce,  just to easy to show
-    }
-}
-
