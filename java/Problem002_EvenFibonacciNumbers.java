da8187aa2d55bc81ec20f585268fe9c546b9e9df
Timo Strating <timostrating@home.nl>
2017-06-04 13:15:35 +0200

    solved problem 2 in java

--- java/Problem002_EvenFibonacciNumbers.java
+++ java/Problem002_EvenFibonacciNumbers.java
@@ -3,6 +3,18 @@
  */
 public class Problem002_EvenFibonacciNumbers {
     public static void main (String args[]) {
-        System.out.println("antwoord = school");
+        int first = 1, second = 1, third = 1;
+        int total = 0;
+
+        while (third < 4_000_000) {
+			third = first + second;
+			first = second;
+			second = third;
+
+			if (second % 2 == 0)
+				total += second;
+		}
+
+		System.out.println("ans = " + total);
     }
 }
