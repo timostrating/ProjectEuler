20f6fccbd3de3e06f04402f028112f1109aa0c91
Timo Strating <timostrating@home.nl>
2017-06-05 01:10:46 +0200

    solved problem 9 in ruby

---
+++ ruby/Problem009_SpecialPythagoreanTriplet.rb
@@ -0,0 +1,16 @@
+500.times { |a|
+	500.times { |b|
+		500.times { |c|
+			if a+b+c == 1000  and  a**2 + b**2 == c**2
+				puts "#{a} + #{b} + #{c} = #{a*b*c}"
+			end
+		}
+	}	
+}
+
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
