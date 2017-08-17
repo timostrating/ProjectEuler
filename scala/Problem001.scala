705ea27e8ee5c638562a2a15eb76150939719bf5
Timo Strating <timostrating@home.nl>
2017-08-18 01:03:23 +0200

    hey that's pretty good

--- scala/Problem001.scala
+++ scala/Problem001.scala
@@ -2,11 +2,5 @@
   * Created by Sneeuwpopsneeuw on 15-Aug-17.
   */
 object Problem001 extends App {
-  var out = 0
-
-  for(i <- 1 until 1000)
-    if(i % 3 == 0  ||  i % 5 == 0)
-      out += i
-
-  println( out )
+  println(((1 until 1000).filter(_ % 3 == 0).toSet  ++  (1 until 1000).filter(_ % 5 == 0).toSet ).sum)
 }
