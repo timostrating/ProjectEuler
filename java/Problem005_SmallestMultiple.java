02f3008636ab2d7242345e24e6bee93bb0eec7a4
Timo Strating <timostrating@home.nl>
2018-01-14 01:36:27 +0100

    explained problem 5

--- java/Problem005_SmallestMultiple.java
+++ java/Problem005_SmallestMultiple.java
@@ -3,15 +3,13 @@ public class Problem005_SmallestMultiple {
 
     public Problem005_SmallestMultiple() {
         int number = 1;
-        for (int i=1; i<20; i++)
-            number = lcm(number, i);
+        for (int i=1; i<20; i++)                                    // LOOP  0 -> 20
+            number = lcm(number, i);                                // LCM(0,1,2,3,4, ...) = ..LCM( LCM( LCM( LCM(0,1), 2), 3, 4)...
 
         System.out.println(number);
     }
 
-    int gcd(int m, int n) {  // English  -  Greatest Common Divisor
-        return (n == 0)? m : gcd(n, m % n);
-    }   // Greatest  Common  Divisor
+    int gcd(int m, int n) { return (n == 0)? m : gcd(n, m % n); }   // Greatest  Common  Divisor
     int lcm(int m, int n) { return(m * n) / gcd(m, n); }            // Least     Common  Multiple
 }
 
