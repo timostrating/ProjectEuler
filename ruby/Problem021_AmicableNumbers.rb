7b413466d94f6756052489e1a552c124d18bffb1
timostrating <timostrating@home.nl>
2018-06-01 23:39:30 +0200

    solved problem 21 in ruby

---
+++ ruby/Problem021_AmicableNumbers.rb
@@ -0,0 +1,30 @@
+require "prime"
+
+MAX = 10000
+
+def proper_deviders_sum input
+    sum = 1
+
+    (2..Math.sqrt(input)).each do |i|
+        if input % i == 0
+            sum += i
+            sum += input / i
+        end
+    end
+
+    return sum
+end
+
+
+sum = 0
+(1...MAX).each do |i|
+    v1 = proper_deviders_sum(i)
+    v2 = proper_deviders_sum(v1)
+
+    if (v1 == v2 && v2 != i)
+        sum += i
+    end
+
+end
+
+puts "sum: #{sum}"
