70d8a8b805b4a90db1eeccf1c9a28597265e1b97
Timo Strating <timostrating@users.noreply.github.com>
2018-09-11 18:31:31 +0200

    Added some comments

--- java/Problem014_LongestCollatzSequence.java
+++ java/Problem014_LongestCollatzSequence.java
@@ -7,17 +7,17 @@ public class Problem014_LongestCollatzSequence {
         int teller = 0;
         int highestTeller = 0;
 
-        for (long i=getal; i > 5; i--) {
+        for (long i=getal; i > 5; i--) {  // loop down from 1 million to 5
             getal = i;
             teller = 0;
-            do {
+            do { // So long as our number is not 1. Update the number and remember how many steps it takes
                 getal = ((getal % 2 == 0))? getal / 2 : getal * 3 + 1;
                 teller++;
             } while (getal > 1);
 
-            if(teller >= highestTeller) {
+            if(teller >= highestTeller) { // if there is a new highscore store it
                 highestTeller = teller;
-                System.out.println(i + " : " + highestTeller);
+                System.out.println(i + " : " + highestTeller); // The last highscore will remain visible in the console
             }
         }
     }
