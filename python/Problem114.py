3f278cae69eabd425139c4ee4260aac2ab49ec26
timostrating <timostrating@home.nl>
2018-11-04 02:04:15 +0100

    improved problem 114

--- python/Problem114.py
+++ python/Problem114.py
@@ -1,37 +1,41 @@
-minimale_auto_lengte = 3
+def memoize(function):
+    from functools import wraps
 
-lookup_table = [0] * 51
+    cache = {}
 
-def counting_block(parkeerplaatsen, geschiedenis=""):   # https://projecteuler.net/problem=114
-    if parkeerplaatsen < minimale_auto_lengte: # We gaan recursief niet verder als we minder dan 3 plaatsen vrij hebben want een auto neemt minimaal 3 plaatsen in
+    @wraps(function)
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            rv = function(*args)
+            cache[args] = rv
+            return rv
+    return wrapper
+
+
+
+min_car_lenth = 3
+
+@memoize
+def counting_block(spaces, first_time=True):  
+    if spaces < min_car_lenth: # less than 3 spots has no solution
         return 0
 
-    if lookup_table[parkeerplaatsen] != 0:
-        return lookup_table[parkeerplaatsen]
-    
     count = 0
-    if geschiedenis == "":                # Als we nog niet recursief ons zelf hebben aangeroepen hebben dan hebben we nog geen geschiedenis
-        parkeerplaatsen += 1  
+    if first_time == True:  # first time running
+        spaces += 1  
 
-    for auto_lengte in range(minimale_auto_lengte, parkeerplaatsen): # Loop alle mogelijke lengtes dus van 3 tot 7 in dit geval
-        for ruimte_links in range(parkeerplaatsen - auto_lengte):       # Stel we willen een auto van 3 lang plaatsen dan kan hij op 7 -3 verschillende plekken beginnen
-            ruimte_recht = parkeerplaatsen - (ruimte_links + auto_lengte + 1)  # Rechts hebben wij nu nog ruimte vrij namelijk de ruimte die overblijft na ruimte links + 1 (omdat je anders niet weg kan rijden) ingenomen is.
+    for auto_lengte in range(min_car_lenth, spaces): # Loop posible car lenths
+        for space_left in range(spaces - auto_lengte):         # loop posible start positions
+            spaceright = spaces - (space_left + auto_lengte + 1)   # calculate the space that is left
+            count += 1 + counting_block(spaceright, False)                    # recursive call
 
-            # print("_"*ruimte_links + geschiedenis, "X"*auto_lengte, "_"*ruimte_recht, sep="")
+    return count + 1 if first_time == True else count
 
-            count += 1 
-            count += counting_block(ruimte_recht, ("_"*ruimte_links) + ("X"*auto_lengte) + "_")  # Roep ons zelf recursief aan en geef de geschidenis door
 
-    if geschiedenis == "":  # we hebben een lege parkeerplaats
-        # print("_"*parkeerplaatsen)
-        return count + 1
-    else:
-        lookup_table[parkeerplaatsen] = count
-        # print(count)
-        return count
-            
 
-print(counting_block(50), "mogelijk heden")   # deze zou een antwoord moeten geven als je 50 in vult in plaats van 7
+print(counting_block(50))
 
 
 
