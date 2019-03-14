5e61f1e2031b174fe636f40ce60dfb6696ccd406
timostrating <timostrating@home.nl>
2019-03-14 23:59:08 +0100

    179

---
+++ python/Problem179.py
@@ -0,0 +1,18 @@
+import math
+
+maxi = 20  # 10**3
+points = [0] * maxi
+
+for n in range(1, maxi +1):
+    i = n
+    while i < maxi:
+        points[i] += 1
+        i += n
+
+sum = 0
+for i in range(len(points) - 1):
+    if points[i] == points[i + 1]:
+        sum += 1
+
+print(points)
+print(sum)  # 986262
