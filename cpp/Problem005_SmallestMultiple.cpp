a52fe926c1080a006d7bc9681a7b09a8c25a4c8f
timostrating <timostrating@home.nl>
2019-08-14 12:49:39 +0200

    solved problem 5 in cpp

---
+++ cpp/Problem005_SmallestMultiple.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#define MAX 20
+using std::cout;
+
+int main()
+{
+    // We simplefy this array towards the array that contains primes and all the dividers
+    int data[MAX] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+    for (int i=1; i<MAX; i++)
+        for (int j=1; j<std::min(i, MAX); j++)
+            if (data[i] % data[j] == 0)
+                data[i] = data[i] / data[j];
+
+    long sum = 1;
+    for(int i=0; i<MAX; i++) // The product of the array is now the answer
+    {
+        sum *= data[i];
+        cout << i+1 << " " << data[i] << "\n";
+    }
+
+    cout << sum << "\n";    
+    return 0;
+}
